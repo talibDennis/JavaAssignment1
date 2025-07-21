bd6f67e5153b2e610bd192a283ff04af8b9b7754
talibDennis <joseph.dennis@triosstudent.com>
2025-07-21 11:58:23 -0400

    Xcode was made to troubleshoot why code wouldn't run

--- Xcode/SampleTest.java
+++ Xcode/SampleTest.java
@@ -1,23 +1,23 @@
-package Ex215;
+package Xcode;
 
 import java.util.Scanner;
 
-public class AdditionTest {
+public class SampleTest {
   public static void main(String[] args) {
-    
+
     Scanner input = new Scanner(System.in);
 
-    System.out.print("Input a number: ");
+    System.out.print("Enter first number: ");
     int num1 = input.nextInt();
 
-    System.out.print("Input a number: ");
+    System.out.print("Enter first number: ");
     int num2 = input.nextInt();
 
     int sum = num1 + num2;
 
-    System.out.printf("Sum is %d%n", sum);
+    System.out.printf("Sum of all is %d%n", sum);
 
     input.close();
   }
-
+  
 }
