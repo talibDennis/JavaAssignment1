7356d32a0a7fd52def1a479706d2130ce8ffdcaf
talibDennis <joseph.dennis@triosstudent.com>
2025-07-21 05:14:44 -0400

    First code commit: Ex215.java

---
+++ Ex215/Ex215.java
@@ -0,0 +1,26 @@
+package Ex215;
+
+public class Ex215 {
+  private int num1;
+  private int num2;
+
+  public Ex215(int num1, int num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  } // constructor
+
+  public int getNum1() {
+    return this.num1;
+  } // getNum1()
+  public int getNum2() {
+    return this.num2;
+  } // getNum2()
+
+  public void setNum1(int num1) {
+    this.num1 = num1;
+  } // setNum1
+  public void setNum2(int num2) {
+    this.num2 = num2;
+  } // setNum2
+
+}
